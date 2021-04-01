35e50f12d3181f0c04b23ae22256bf1e326347ca
Milos Tijanic <mikigrobar1997@gmail.com>
2021-04-01 16:23:44 +0200

    svee

--- ind.php
+++ ind.php
@@ -1,5 +1,13 @@
 <?php
  echo ("biti");
- echo(ne biti);
-
+ echo("ne biti");
+echo("pitanje je");
 ?>
+
+
+<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>">
+
+<input name="nesto" type="text" value="type..."/>
+<input name="nesto" type="text" value="author"/>
+<input type="submit" value="add"/>
+</form>
